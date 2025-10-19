6c9653871afa399b300b5cb60589139516ad674b
Cervelen <cervelenalias@gmail.com>
2025-10-19 16:17:49 -0700

    continuing to my laptop

--- FDTD/fdtd.cpp
+++ FDTD/fdtd.cpp
@@ -7,3 +7,15 @@
 
 
 
+Vorp::Vorp(std::string e_or_h, std::string direction, double permeability, double starting_magnitude){
+    this->e_or_h = e_or_h;
+    this->direction = direction;
+    this->permeability = permeability;
+    this->magnitude = starting_magnitude;
+    this->sim_values = {};
+}
+
+void Vorp::change_magnitude(double magnitude){
+    this->magnitude = magnitude;
+}
+
--- FDTD/fdtd.h
+++ FDTD/fdtd.h
@@ -3,7 +3,24 @@
 
 #include <vector>
 
+class Vorp{
+    public:
+        std::string e_or_h;         //classify whether an E field value or H field value
+        std::string direction;      //direction the E or H field goes
+        double permeability;        //permeability of the material this point is a part of
+        double magnitude;
+        std::vector<double> sim_values;
 
+        Vorp(std::string e_or_h, std::string direction, double permeability, double magnitude);
+        void change_magnitude(double magnitude);
+
+};
+
+class E_Sim{
+    public:
+        std::vector<std::vector<std::vector<Vorp>>> grid;   //each vorp in the grid is separated by 1/2 cell from each other vorp.
+
+}
 
 
 
