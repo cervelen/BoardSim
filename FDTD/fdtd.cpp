555c8b6e4fc8c6014ef6a66bd699697b77caa434
Cervelen <cervelenalias@gmail.com>
2025-10-19 21:44:15 -0700

    more more more

--- FDTD/fdtd.cpp
+++ FDTD/fdtd.cpp
@@ -7,17 +7,41 @@
 
 
 
-Vorp::Vorp(std::string e_or_h, std::string direction, double permeability, double starting_magnitude){
+Vorp::Vorp( double permeability, double e_or_h, double direction, double starting_magnitude){
     this->e_or_h = e_or_h;
     this->direction = direction;
     this->permeability = permeability;
     this->magnitude = starting_magnitude;
-    this->sim_values = {};
+    this->sim_magnitudes = {};
 }
 
 void Vorp::change_magnitude(double magnitude){
     this->magnitude = magnitude;
 }
 
+E_Sim::E_Sim(std::vector<std::vector<std::vector<std::vector<double>>>> input_grid){
+
+    //Construction below
+    for (int layer = 0; layer < input_grid.size(); layer++){
+        this->grid[layer].push_back({});
+        for (int row = 0; row < input_grid[layer].size(); row++){
+            this->grid[layer][row].push_back({});
+            for (int vorp = 0; vorp < input_grid[layer][row].size(); vorp++){
+                this->grid[layer][row].emplace_back(Vorp(input_grid[layer][row][vorp][0], input_grid[layer][row][vorp][1], input_grid[layer][row][vorp][2], 0));
+            }
+        }
+    }
+    //construction above
+    //validation below
+    for (int layer = 0; layer < input_grid.size(); layer++){
+        for (int row = 0; row < input_grid[layer].size(); row++){
+            for (int vorp = 0; vorp < input_grid[layer][row].size(); vorp++){
+                this->grid[layer][row].emplace_back(Vorp(input_grid[layer][row][vorp][0], input_grid[layer][row][vorp][1], input_grid[layer][row][vorp][2], 0));
+            }
+        }
+    }
+
+
+}
 
 
--- FDTD/fdtd.h
+++ FDTD/fdtd.h
@@ -5,13 +5,13 @@
 
 class Vorp{
     public:
-        std::string e_or_h;         //classify whether an E field value or H field value
-        std::string direction;      //direction the E or H field goes
-        double permeability;        //permeability of the material this point is a part of
-        double magnitude;
-        std::vector<double> sim_values;
+        double e_or_h;              //classify whether an E field value or H field value. 0 -> E, 1 -> H.
+        double direction;           //direction the E or H field is referenced. 1 -> x, 2 -> y, 3 -> z.
+        double permeability;        //permeability of the material, of which this point is a part.
+        double magnitude;           //magnitude that changes as the field changes.
+        std::vector<double> sim_magnitudes;
 
-        Vorp(std::string e_or_h, std::string direction, double permeability, double magnitude);
+        Vorp(double e_or_h, double direction, double permeability, double magnitude);
         void change_magnitude(double magnitude);
 
 };
@@ -21,8 +21,9 @@ class E_Sim{
         std::vector<std::vector<std::vector<Vorp>>> grid;   //each vorp in the grid is separated by 1/2 cell from each other vorp.
         
 
-        E_Sim(std::vector<std::vector<std::vector<double>>> )
+        E_Sim(std::vector<std::vector<std::vector<std::vector<double>>>> input_grid); //each 3rd depth vector has 3 doubles: 0th -> permeability, 1st -> E or H, 2nd -> direction. 
 };
+
 
 
 
