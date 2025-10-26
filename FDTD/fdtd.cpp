a11462e8bd2483a289d683f2ae1a043df5e4d80a
Cervelen <cervelenalias@gmail.com>
2025-10-26 15:29:15 -0700

    refresh

--- FDTD/fdtd.cpp
+++ FDTD/fdtd.cpp
@@ -6,6 +6,23 @@
 #include <functional>
 #include <cmath>
 
+double3::double3(double x, double y, double z) : x(x), y(y), z(z) {}
+
+double3 double3::operator+(const double3& v) const {
+    return double3(x + v.x, y + v.y, z + v.z);
+}
+
+double3 double3::operator-(const double3& v) const {
+    return double3(x - v.x, y - v.y, z - v.z);
+}
+
+double3 double3::operator*(double s) const {
+    return double3(x * s, y * s, z * s);
+}
+
+double double3::length() const {
+    return std::sqrt(x * x + y * y + z * z);
+}
 
 
 Vorp::Vorp( double permeability, double e_or_h, double direction, double starting_magnitude){
@@ -21,51 +38,10 @@ void Vorp::change_magnitude(double magnitude){
 }
 
 E_Sim::E_Sim(std::vector<std::vector<std::vector<std::vector<double>>>> input_grid){
-    //Construction below
-    // for (int layer = 0; layer < input_grid.size(); layer++){
-    //     this->grid[layer].push_back({});
-    //     for (int row = 0; row < input_grid[layer].size(); row++){
-    //         this->grid[layer][row].push_back({});
-    //         for (int vorp = 0; vorp < input_grid[layer][row].size(); vorp++){
-    //             this->grid[layer][row].emplace_back(Vorp(input_grid[layer][row][vorp][0], input_grid[layer][row][vorp][1], input_grid[layer][row][vorp][2], 0));
-    //         }
-    //     }
-    // }
-    //construction above
-    //validation below
-    //validation above
-}
-
-Fuihua::Fuihua(const std::string& simulation_type, double voxel_scalar){
-    this->simulation_type = simulation_type;
-    this->voxel_scalar = voxel_scalar;
-    this->face_ids = {};
-    this->voxelized_grid_size = {0.0, 0.0, 0.0};
-
-    std::vector<std::vector<double>> sim_object = {};
-    this->simulation_object = sim_object;
-    std::vector<std::vector<std::vector<double>>> vox_grid = {};
-    this->voxelized_grid = vox_grid;
-}
-
-void Fuihua::generate_material(double material_positive, double material_negative, std::vector<std::vector<double>>& faces){
-    //initial pass
-    if (face_ids.empty() == true){
-        for (int face = 0; face < faces.size(); face++){
-            face_ids.push_back(static_cast<double>(face));
-
-            double* numerator = faces[face].data();         //generates array to be passed into function
-
-            std::vector<double> denominator_vector(faces[face].size(), voxel_scalar);
-            double* denominator = denominator_vector.data();
-
-            std::vector<double> result_vector(faces[face].size(), 0.0);
-            double* result = result_vector.data();
-
-            divide_vectors(numerator, denominator, result, result_vector.size());
-
-
-        }
-    }
 
 }
+
+Fuihua::Fuihua(){
+
+}
+
--- FDTD/fdtd.h
+++ FDTD/fdtd.h
@@ -3,6 +3,21 @@
 
 #include <vector>
 
+
+
+struct double3 {
+    double x, y, z;
+    
+    double3(double x, double y, double z);
+    
+    double3 operator+(const double3& v) const;
+    double3 operator-(const double3& v) const;
+    double3 operator*(double s) const;
+    double length() const;
+};
+
+
+
 class Vorp{
     public:
         double e_or_h;              //classify whether an E field value or H field value. 0 -> E, 1 -> H.
@@ -26,46 +41,9 @@ class E_Sim{
 
 class Fuihua{
     public:
-        //the simulation object is a 3d shape that has varying materials enclosed within.
-        //Each 1st depth vector is a face.
-        //each face is created by a vector of doubles, where:
-        //0th index: material normal positive to the surface.
-        //1st index: material normal negative to the surface.
-        //material 0 will ALWAYS be outside the simulation environment.
-        //4th index: number of vertices.
-        //3rd-11th+ index:  xyz, xyz, xyz... of each vertex. 
-        //xyz location will always be spatially in METERS.
-        //if you want to avoid bad values, normalize your neural network, NOT the simulation. This is done for accuracies sake.
-        //vertices are all connected in a clockwise pattern when looking from above (looking in the negative direction).
-        //boundary oriented simulation. 
-        //The material normal positive to EVERY face in the environment box is the same, and the boundary MUST encompass everything being simulated.
-        //This massive object is done to preserve efficiency of simulation.
-        //I implore you to play around and make things PARAMETRICALLY.
-        //don't forget to have fun and be creative, make machines, advance humanity, and support your community.
-        std::vector<std::vector<double>> simulation_object;
-        std::string simulation_type;
-        std::vector<std::vector<std::vector<double>>> voxelized_grid;
-        std::vector<double> voxelized_grid_size; //format x, y, z.
-        std::vector<double> face_ids;
-        double voxel_scalar;
 
-        Fuihua(const std::string& simulation_type, double voxel_scalar);
 
-        //if a generated material overlaps with another, it overwrites that material inherently.
-        //The order of material generation will determine the object -> simulation conversion.
-
-        //this function is for building with your own sim environment, and meant to be used the most.
-        void generate_material(double material_positive, double material_negative, std::vector<std::vector<double>>& faces);
-
-        //these below are for QOL and tutorials
-        void generate_box(double origin_x, double origin_y, double origin_z, double length, double width, double height); //x -> length, y -> width, z -> height
-        void generate_sphere(double origin_x, double origin_y, double origin_z, double r, double theta, double phi); 
-
-        //Below, each punch is represented in the same way as a face, but without material (since there is no material there).
-        //First face is outside punch, each additional is a hole in the plate.
-        //this function is meant for simulations of traditional layered PCBs. 
-        void generate_plate(double origin_x, double origin_y, double origin_z, std::vector<std::vector<double>> punches); 
-
+        Fuihua();
 
 };
 
