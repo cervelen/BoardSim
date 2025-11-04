bd67df63daae49bc94a68c80a0239111efe004fc
Cervelen <cervelenalias@gmail.com>
2025-11-03 22:54:58 -0800

    learning more

--- FDTD/experiment/fdtd_cuda_experiment.cu
+++ FDTD/experiment/fdtd_cuda_experiment.cu
@@ -3,6 +3,24 @@
 
 
 
+D bool box::in_box(double3 point){
+    return true;
+}
+
+__global__ void check_in_box(box my_box, double3* points, int n){
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < n) {
+        my_box.in_box(points[idx]);
+    }
+}
+
+void launch_check_in_box(box my_box, double3* points, int n){
+    int blockSize = 256;
+    int gridSize = (n + blockSize - 1) / blockSize;
+    check_in_box<<<gridSize, blockSize>>>(my_box, points, n);
+    cudaDeviceSynchronize();
+}
+
 D bool sphere::in_sphere(double3 point){
     return true;
 }
--- FDTD/experiment/fdtd_experiment.h
+++ FDTD/experiment/fdtd_experiment.h
@@ -12,6 +12,8 @@
 #define __global__
 #endif
 
+
+
 struct double3 {
     double x, y, z;
     
@@ -23,18 +25,50 @@ struct double3 {
     }
 };
 
-struct sphere{
+struct box {
     double3 origin;
-    double3 rtp; //roe, theta, phi
+    double3 lwh;
     double3 dirvector;
 
-    D sphere(double3 origin_, double3 rtp_, double3 dirvector_) : origin(origin_), rtp(rtp_), dirvector(dirvector_) {}
+    HD box(double3 origin_, double3 lwh_, double3 dirvector_) : origin(origin_), lwh(lwh_), dirvector(dirvector_) {}
+    D bool in_box(double3 point);
+
+}
+
+struct sphere{
+    double3 origin;
+    double3 rtp;        //roe, theta, phi
+    double3 dirvector;
+
+    HD sphere(double3 origin_, double3 rtp_, double3 dirvector_) : origin(origin_), rtp(rtp_), dirvector(dirvector_) {}
     D bool in_sphere(double3 point);
 };
 
 void launch_check_in_sphere(sphere my_sphere, double3* points, int n);
 
+void launch_check_in_box(box my_box, double3* points, int n);
 
+struct vorp{
+    double3 location;
+    double3 dirvector;
+    int h_e;            //h -> magnetic field, e -> electric field
+    int material = 0;   //each material associated with a materialid, allowing the characteristics to be known for simulation (0 is free space)
+
+    HD vorp(double3 location_, double3 dirvector_, int h_e_) : location(location_), dirvector(dirvector_), h_e(h_e_) {}
+
+
+}
+
+struct fuihua{
+    std::vector<std::variant<sphere, box>> elements;    //first element is the sim box
+    std::vector<vorp> fdtd_field;                       //vorps are put in vector irrespective of position
+
+    H fuihua() : {}
+    H place_box(double3 origin, double3 lwh, double3 dirvector);
+    H place_sphere(double3 origin, double3 rtp, double3 dirvector);
+
+    H 
+};
 
 
 
